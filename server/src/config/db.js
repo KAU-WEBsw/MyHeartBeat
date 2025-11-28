1b693df368ecd0d6c253a28319d7eea43aa6812d
minseo23232 <ams4251234@naver.com>
2025-11-28 21:47:45 +0900

    수정

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,15 +1,10 @@
-const mysql = require("mysql2/promise");
 const path = require("path");
-
-// 🔥 server/.env 자동 로드
-require("dotenv").config({ path: path.resolve(__dirname, "../../.env") });
-
+require("dotenv").config({ path: path.join(__dirname, "../../.env") });
+const mysql = require("mysql2/promise");
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
 });
-
 module.exports = pool;
